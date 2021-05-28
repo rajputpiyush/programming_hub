9ad835942962dea35ecc52bb87c4c3dea1b7b1bd
rajputpiyush <rajputpiyush07july@gmail.com>
2021-05-28 21:41:07 +0530

    add bfs in unweighted_graph_adjacency_list.java file:

--- unweighted_graph_adjacency_list.java
+++ unweighted_graph_adjacency_list.java
@@ -1,6 +1,9 @@
 import java.util.List;
+import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.Arrays;
 public class unweighted_graph_adjacency_list{
     public static void main(String args[]){
         unweighted_graph graph = new unweighted_graph(4);
@@ -8,7 +11,12 @@ public class unweighted_graph_adjacency_list{
         graph.add_bidirectional(1 , 2);
         graph.add_bidirectional(2 , 3);
         graph.add_bidirectional(0 , 3);
+        System.out.println("This is dfs search using graph");
+        System.out.println();
         graph.dfs(0);
+        System.out.println("This is bfs seach using graph");
+        System.out.println();
+        graph.bfs(0);
     }
 }
 final class unweighted_graph{ // this class is a final class which can't be inherited
@@ -30,8 +38,11 @@ final class unweighted_graph{ // this class is a final class which can't be inhe
         graph.get(source).add(destination); 
         graph.get(destination).add(source);
     }
-    public void dfs(int source){
-        Stack<Integer> stack = new Stack<Integer>();
+    public void dfs(int source){ // this is used to bfs traversal in graph
+        Stack<Integer> stack = new Stack<Integer>(); // we use stack for dfs
+        Arrays.fill(visited , false); 
+        // this is used for case like when if we perform bfs first and after try to implement
+        //  dfs then visited array is ture already during bfs so for this condition we fill this array false
         stack.push(source);
         visited[source] = true;
         while(!stack.isEmpty()){
@@ -45,6 +56,26 @@ final class unweighted_graph{ // this class is a final class which can't be inhe
                 }
             }
         }
+    }
+    public void bfs(int source){
+        Queue<Integer> queue = new LinkedList<Integer>(); // remember we use queue for bfs
+        // this is used for case like when if we perform dfs first and after try to implement
+        //  bfs then visited array is ture already during bfs so for this condition we fill this array false
+        Arrays.fill(visited, false);
+        queue.add(source);
+        visited[source] = true;
+        while(!queue.isEmpty()){
+            Integer pop = queue.poll();
+            System.out.println(pop);
+            List<Integer> container = graph.get(pop);
+            for(Integer child: container){
+                if(!visited[child]){
+                    queue.add(child);
+                    visited[child] = true;
+                }
+            }
+        }
+
     }
     
 }
