fd640353dffca790a8c7d54fb90ebe488addfc60
rajputpiyush <rajputpiyush07july@gmail.com>
2021-06-02 21:01:48 +0530

    add level order travering in binary tree using queue

--- famous_data_structure/binary_tree.java
+++ famous_data_structure/binary_tree.java
@@ -1,5 +1,8 @@
-
-
+// in this binary_tree i implement two types of traversal
+// 1) DFS Traversal - preorder traversal , inorder traversal , postorder traversal
+// 2) BFS Traversal / level order traversing
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 public class binary_tree{
     public static void main(String args[]){
@@ -13,8 +16,11 @@ public class binary_tree{
         tree.Preorder_traversal(1);
         System.out.println("Postorder traversal : ");
         tree.Postorder_traversal(2);
+        System.out.println("Level Order traversal : ");
+        tree.Levelorder_traversal(0);
         System.out.println("Get root node : ");
         System.out.println(tree.get_root());
+
   
     }
 }
@@ -138,7 +144,37 @@ class B_Tree{
         Post_traversal(node.right);
         System.out.println(node.data);
     }
-
+    
+    public void Levelorder_traversal(int start){
+        Node temp = head;
+        Levelorder_traversal(temp , start);
+    }
+    private Node Levelorder_traversal(Node node , int start){
+        if(node == null){
+            return null;
+        }
+        if(node.data == start){
+            L_traversal(node);
+        }
+        Levelorder_traversal(node.left , start);
+        Levelorder_traversal(node.right , start);
+        return node;
+    }
+    private Node L_traversal(Node node){
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node nodes = queue.poll();
+            System.out.println(nodes.data);
+            if(nodes.left != null) {
+                queue.add(nodes.left);
+            }
+            if(nodes.right != null) {
+        	    queue.add(nodes.right);        	 
+            }     
+        }
+        return node;
+    }
 }
 class Node{
     int data;
